815e5a5c64aba0f878c3fb203f605c8dcfd799fe
Ahmad <mahmad.wasim@systemsltd.com>
2025-08-17 01:01:28 -0700

    Update AWS region, AMI ID, and instance type descriptions in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "aws_region" {
-  description = "AWS region to deploy EC2"
-  default     = "ap-south-1"
+  description = "AWS region to launch instance in"
+  type        = string
+  default     = "eu-north-1" # Stockholm
 }
 
 variable "ami_id" {
-  description = "AMI ID for EC2 (Amazon Linux 2)"
-  default     = "ami-0cca134ec43cf708f" # ap-south-1 Amazon Linux 2 AMI
+  description = "AMI ID for Ubuntu 22.04 in Stockholm"
+  type        = string
+  default     = "ami-01b64b6deccb0e0a2" # check if valid in eu-north-1
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Instance type"
+  type        = string
   default     = "t3.micro"
 }
